bb1ac158ea21696c074b05a7d4f06b066bda1c9d
Duynguyen162 <duynguyen162204@gmail.com>
2025-07-07 14:59:54 +0700

    cài tailwind

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import GlobalStyles from '~/components/GlobalStyles';
 import React from 'react';
-
+import './tailwind.css';
 const rootElement = document.getElementById('root');
 
 if (!rootElement) throw new Error('Failed to find the root element');
